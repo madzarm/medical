90605c3cbaa09444e101b6e582e560494dec900d
Maksim <madzarmaksim@gmail.com>
2022-08-24 15:47:48 +0200

    added AsyncConfiguration

---
+++ medical-api/src/main/java/com/example/medicalapi/conf/AsyncConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.medicalapi.conf;
+
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfiguration {
+
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(AsyncConfiguration.class);
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        LOGGER.debug("Creating Async Task Executor");
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("UserThread-");
+        executor.initialize();
+        return executor;
+    }
+}
