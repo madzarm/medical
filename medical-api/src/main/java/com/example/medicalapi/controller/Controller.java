8842a233843506076995b20e16d9239e82a7e553
Maksim <madzarmaksim@gmail.com>
2022-08-24 12:58:06 +0200

    added Controller and Service

---
+++ medical-api/src/main/java/com/example/medicalapi/controller/Controller.java
@@ -0,0 +1,5 @@
+package com.example.medicalapi.controller;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+}
---
+++ medical-api/src/main/java/com/example/medicalapi/service/Service.java
@@ -0,0 +1,5 @@
+package com.example.medicalapi.service;
+
+@org.springframework.stereotype.Service
+public class Service {
+}
