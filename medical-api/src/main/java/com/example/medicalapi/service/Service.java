8b49b36c937180a6f3c4b559a6450bebc6c4ea5f
Maksim <madzarmaksim@gmail.com>
2022-08-24 15:02:08 +0200

    added test method

--- medical-api/src/main/java/com/example/medicalapi/service/Service.java
+++ medical-api/src/main/java/com/example/medicalapi/service/Service.java
@@ -12,6 +12,22 @@ import java.util.List;
 @org.springframework.stereotype.Service
 public class Service {
 
+
+    public void findByUserId(){
+        JSONObject obj = new JSONObject();
+        obj.put("userId",Integer.valueOf(1));
+        obj.put("firstName","Jack");
+        obj.put("lastName","Jackson");
+        obj.put("weight", Float.valueOf(76.3f));
+        obj.put("age",Integer.valueOf(37));
+        obj.put("diseases", List.of("disease1, disease2, disease3"));
+        System.out.println(obj.toJSONString());
+        MedicalRecord mr = jsonToPojo(obj);
+        System.out.println(mr);
+    }
+
+
+
     public MedicalRecord jsonToPojo(JSONObject jsonObject){
         ObjectMapper mapper = new ObjectMapper().registerModule(new JsonOrgModule());
         return mapper.convertValue(jsonObject, MedicalRecord.class);
