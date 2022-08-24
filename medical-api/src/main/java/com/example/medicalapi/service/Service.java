524f054537a11eef6f21f9db7bfcf5b9ce491f13
Maksim <madzarmaksim@gmail.com>
2022-08-24 14:56:34 +0200

    jsonToPojo mapper added

--- medical-api/src/main/java/com/example/medicalapi/service/Service.java
+++ medical-api/src/main/java/com/example/medicalapi/service/Service.java
@@ -1,5 +1,19 @@
 package com.example.medicalapi.service;
 
+import com.example.medicalapi.domain.MedicalRecord;
+import com.example.medicalapi.service.result.DataResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsonorg.JsonOrgModule;
+import org.json.simple.JSONObject;
+
+import javax.json.JsonObject;
+import java.util.List;
+
 @org.springframework.stereotype.Service
 public class Service {
+
+    public MedicalRecord jsonToPojo(JSONObject jsonObject){
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JsonOrgModule());
+        return mapper.convertValue(jsonObject, MedicalRecord.class);
+    }
 }
