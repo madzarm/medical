95116a09e3ffc78d42f2a1651364e9a576922b3b
Maksim <madzarmaksim@gmail.com>
2022-08-25 10:07:28 +0200

    added security

---
+++ medical-api/src/main/java/com/example/medicalapi/security/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.example.medicalapi.security;
+
+import com.example.medicalapi.domain.User;
+import com.example.medicalapi.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(username);
+        if(user.isEmpty())
+            throw new UsernameNotFoundException("Not found: " + username);
+        return user.map(MyUserDetails::new).get();
+    }
+}
---
+++ medical-api/src/main/java/com/example/medicalapi/security/SecurityConfiguration.java
@@ -0,0 +1,59 @@
+package com.example.medicalapi.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfiguration(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService2());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/**").hasRole("USER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+
+    @Bean
+    protected UserDetailsService userDetailsService2() {
+        UserDetails firstUser = User.builder()
+                .username("username")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(
+                firstUser
+        );
+    }
+
+}
