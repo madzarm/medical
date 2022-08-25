026a8cc81e85a905951dac7788ea05d2392785e7
Maksim <madzarmaksim@gmail.com>
2022-08-25 10:28:58 +0200

    added PersonDTO

---
+++ medical-api/src/main/java/com/example/medicalapi/domain/PersonDTO.java
@@ -0,0 +1,9 @@
+package com.example.medicalapi.domain;
+
+public class PersonDTO {
+    private int id;
+    private String name;
+    private String surname;
+    private int weight;
+    private int age;
+}
