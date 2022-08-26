d640d9e86beb61a6624a589bc34d0787accde81b
Maksim <madzarmaksim@gmail.com>
2022-08-26 13:28:39 +0200

    added response

--- medical-api/src/main/java/com/example/medicalapi/controller/Controller.java
+++ medical-api/src/main/java/com/example/medicalapi/controller/Controller.java
@@ -1,15 +1,10 @@
 package com.example.medicalapi.controller;
 
-import com.example.medicalapi.domain.MedicalRecord;
 import com.example.medicalapi.service.Service;
-import com.example.medicalapi.service.result.DataResult;
-import org.springframework.boot.actuate.health.Health;
+import com.example.medicalapi.service.result.ActionResult;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -22,8 +17,8 @@ public class Controller {
     }
 
     @GetMapping("/load")
-    public void fetchAndLoad() throws ExecutionException, InterruptedException {
-        service.fetchAndLoad();
+    public ResponseEntity<ActionResult> fetchAndLoad() throws ExecutionException, InterruptedException {
+        return service.fetchAndLoad().intoResponseEntity();
     }
 
 }
