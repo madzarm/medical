d44c7619952ac823352c975d534319a89712e5fe
Maksim <madzarmaksim@gmail.com>
2022-08-26 15:01:42 +0200

    removed unnecessary imports

--- medical-api/src/main/java/com/example/medicalapi/controller/Controller.java
+++ medical-api/src/main/java/com/example/medicalapi/controller/Controller.java
@@ -1,8 +1,6 @@
 package com.example.medicalapi.controller;
 
-import com.example.medicalapi.domain.MedicalRecord;
 import com.example.medicalapi.service.Service;
-import com.example.medicalapi.service.result.ActionResult;
 import com.example.medicalapi.service.result.DataResult;
 import com.example.medicalapi.service.result.SearchMedicalRecordResult;
 import org.springframework.cache.annotation.Cacheable;
@@ -10,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @RestController
